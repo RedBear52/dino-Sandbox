90d8a7639f6150dc44556d33ab221fb0b9e6929b
ryan-spearman <ryanspearman@gmail.com>
2022-02-18 12:16:37 -0600

    Created and Randomized Comparison Functions
    compare Height, Weight, or Diet

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ form.addEventListener('submit', function (event) {
 // TODO need to validate input or, at very least, require input in all fields 
 });
 
+// function randComp(input) {
+//   const compArray = [compareDiet(input), compareHeight(input), compareWeight(input)] 
+//   const randomNumber = Math.floor(Math.random(0, 2))
+//   return compArray[randomNumber]
+// }
 //instantiate Human object from user input
 function buildHuman () {
   let name = document.querySelector("#user_name").value
@@ -46,8 +51,18 @@ function buildHuman () {
   let species = "human"
   let fact = ""
   const userData = new Creature(species, height, weight, image, diet, fact, name);
-  return populateGrid(userData)
-};
+  const randomNumber = parseFloat(Math.floor(Math.random() * 3))
+  switch (randomNumber) {
+    case 0: 
+      compareDiet(userData);
+      break;
+    case 1: 
+      compareWeight(userData);
+      break;
+    case 2: 
+      compareHeight(userData);
+  }
+}; 
 
 // instantiate separate pigeon object and push to the end of creatureArray
 function buildPigeon() {
@@ -60,10 +75,13 @@ function clearForm() {
   document.getElementById('form_container').outerHTML=''
 };
 
-
 //Shuffle array using sort method, this seems a cleaner approach than the Fisher-Yates modern shuffle algorithm 
 function shuffleCreatures () {
   creatureArray.sort((a,b) => 0.5 - Math.random())
+}
+
+function shuffleComps () {
+  compArray.sort((a,b) => 0.5 - Math.random())
 }
 
 //a function to dynamically generate a 3x3 grid and populate it with shuffled dinos, user info, and bird instance
@@ -81,21 +99,130 @@ function populateGrid(humanSpecs) {
       let newFrame = newRow.appendChild(newCell)
       let newPic = document.createElement('img')
       newPic.src = creatureArray[index].image
+      // renderFacts() 
+        if (index === 4) {
+          newCell.innerHTML+= `<h3>${creatureArray[index].name}</h3> `
+          newFrame.appendChild(newPic)
+          //TODO comment  out the line below before submitting!
+          newCell.innerHTML+= "<br>" + `${creatureArray[index].name} is a human with an interest in dinosaurs!`
+          index +=1
+        } else {
+          newCell.innerHTML+= `<h3>${creatureArray[index].species}</h3> `
+          newFrame.appendChild(newPic)
+          newCell.innerHTML+= "<br>" + creatureArray[index].fact
+          index += 1          
+      }
+    }
+  }
+};
+
+function compareHeight(humanSpecs) {
+  shuffleCreatures()
+  creatureArray.splice(4, 0, humanSpecs)
+  buildPigeon()
+  let infoGraphic = document.getElementById('grid')
+  let index = 0
+  for (let r = 0; r < 3; r++) {
+  let newRow = document.createElement('tr')
+  infoGraphic.appendChild(newRow)
+    for (let c = 0; c < 3; c++) {
+      let newCell = document.createElement('td')
+      let newFrame = newRow.appendChild(newCell)
+      let newPic = document.createElement('img')
+      newPic.src = creatureArray[index].image
+      // renderFacts() 
+        if (index === 4) {
+          newCell.innerHTML+= `<h3>${creatureArray[index].name}</h3> `
+          newFrame.appendChild(newPic)
+          newCell.innerHTML+= "<br>" + creatureArray[index].fact
+          index +=1
+        } else {
+          newCell.innerHTML+= `<h3>${creatureArray[index].species}</h3> `
+          newFrame.appendChild(newPic)
+          if (humanSpecs.height === creatureArray[index].height) {
+            newCell.innerHTML+= "<br>" + "What are the chances?!" + "<br>" + `Both you and ${creatureArray[index].species} are exactly ${humanSpecs.height} inches tall`
+            index += 1
+          } else if (humanSpecs.height > creatureArray[index].height) {
+            newCell.innerHTML+= 
+            "<br>" + `You are ${humanSpecs.height - creatureArray[index].height} inches taller than a ${creatureArray[index].species}`
+            index +=1
+          } else if (humanSpecs.height < creatureArray[index].height) {
+            newCell.innerHTML+= 
+            "<br>" + `A ${creatureArray[index].species} measures ${creatureArray[index].height - humanSpecs.height} inches taller than you`
+            index += 1           
+          }
+      }
+    }
+  }
+};
+
+function compareDiet(humanSpecs) {
+  shuffleCreatures()
+  creatureArray.splice(4, 0, humanSpecs)
+  buildPigeon()
+  let infoGraphic = document.getElementById('grid')
+  let index = 0
+  for (let r = 0; r < 3; r++) {
+  let newRow = document.createElement('tr')
+  infoGraphic.appendChild(newRow)
+    for (let c = 0; c < 3; c++) {
+      let newCell = document.createElement('td')
+      let newFrame = newRow.appendChild(newCell)
+      let newPic = document.createElement('img')
+      newPic.src = creatureArray[index].image
+      // renderFacts() 
+        if (index === 4) {
+          newCell.innerHTML+= `<h3>${creatureArray[index].name}</h3> `
+          newFrame.appendChild(newPic)
+          newCell.innerHTML+= "<br>" + creatureArray[index].fact
+          index +=1
+        } else {
+          newCell.innerHTML+= `<h3>${creatureArray[index].species}</h3> `
+          newFrame.appendChild(newPic)
+            if (humanSpecs.diet.toLowerCase() === creatureArray[index].diet) {
+              newCell.innerHTML+= "<br>" + `You and ${creatureArray[index].species} share the same eating style as both of you are ${humanSpecs.diet}s`
+              index +=1
+            } else {
+              newCell.innerHTML+= "<br>" + `Contrary to your ${humanSpecs.diet}'s diet, ${creatureArray[index].species} is all about that ${creatureArray[index].diet} life`
+              index += 1           
+            }
+      }
+    }
+  }
+};
+
+function compareWeight(humanSpecs) {
+  shuffleCreatures()
+  creatureArray.splice(4, 0, humanSpecs)
+  buildPigeon()
+  let infoGraphic = document.getElementById('grid')
+  let index = 0
+  for (let r = 0; r < 3; r++) {
+  let newRow = document.createElement('tr')
+  infoGraphic.appendChild(newRow)
+    for (let c = 0; c < 3; c++) {
+      let newCell = document.createElement('td')
+      let newFrame = newRow.appendChild(newCell)
+      let newPic = document.createElement('img')
+      newPic.src = creatureArray[index].image
+      // renderFacts() 
       if (index === 4) {
         newCell.innerHTML+= `<h3>${creatureArray[index].name}</h3> `
         newFrame.appendChild(newPic)
         //TODO comment  out the line below before submitting!
-        newCell.innerHTML+= "<br>" + `${creatureArray[index].name} is a human with an interest in dinosaurs!`
+        newCell.innerHTML+= "<br>" + creatureArray[index].fact
         index +=1
       } else {
-        newCell.innerHTML+= `<h3>${creatureArray[index].species}</h3> `
+        newCell.innerHTML+= "<br>" + `<h3>${creatureArray[index].species}</h3> `
         newFrame.appendChild(newPic)
-        newCell.innerHTML+= "<br>" + creatureArray[index].fact
-        index += 1
-      }     
+        newCell.innerHTML += "<br>" + `${creatureArray[index].species} weighs in at ${creatureArray[index].weight} pounds compared to you at ${humanSpecs.weight}`
+        index += 1          
     }
   }
-};
+}
+};  
+      
+
 
 
 
